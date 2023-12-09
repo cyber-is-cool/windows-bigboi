c541c69b7dc347c9d16c463fed17bc3e465dffc9
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-12-08 20:29:02 -0600

    Update runing_risk.ps1

--- scripts/runing_risk.ps1
+++ scripts/runing_risk.ps1
@@ -18,6 +18,4 @@ gpupdate /force
 pause
 pause
 
-Install-Module PSWindowsUpdate
-Add-WUServiceManager -MicrosoftUpdate
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
+
