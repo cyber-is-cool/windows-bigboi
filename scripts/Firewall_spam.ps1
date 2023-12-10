c40b2f504aad1127e65df5d5f6f982c7ebd14144
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-12-10 06:56:38 -0600

    Update Firewall_spam.ps1

--- scripts/Firewall_spam.ps1
+++ scripts/Firewall_spam.ps1
@@ -1,1 +1,17 @@
 
+    netsh advfirewall import "$PSScriptRoot/tools/Win10Firewall.wfw"
+    netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Telnet Server" new enable=no 
+    netsh advfirewall firewall set rule name="netcat" new enable=no
+    #disable network discovery hopefully
+    netsh advfirewall firewall set rule group="Network Discovery" new enable=No
+    #disable file and printer sharing hopefully
+    netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No
+
+    netsh advfirewall firewall add rule name="block_RemoteRegistry_in" dir=in service="RemoteRegistry" action=block enable=yes
+    netsh advfirewall firewall add rule name="block_RemoteRegistry_out" dir=out service="RemoteRegistry" action=block enable=yes
